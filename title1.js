d0a5cbf146ce38a4b1abc385769412b0ab83d57e
yongkokhong96 <80205661+yongkokhong96@users.noreply.github.com>
2021-03-14 19:58:07 +0800

    Update title1.js

--- title1.js
+++ title1.js
@@ -3,17 +3,17 @@ var infoCurrentSeason = document.getElementById("selectedSeason");
 
 	function changeSeason(){
 		console.log(infoCurrentSeason.value);
-		if (infoCurrentSeason.value == 10){
+		if (infoCurrentSeason.value == 1){
 			document.getElementById("info-s1").style.display = "block";
 			document.getElementById("info-s2").style.display = "none";
 			document.getElementById("info-s3").style.display = "none";
 		}
-		else if (infoCurrentSeason.value == 20){
+		else if (infoCurrentSeason.value == 2){
 			document.getElementById("info-s1").style.display = "none";
 			document.getElementById("info-s2").style.display = "block";
 			document.getElementById("info-s3").style.display = "none";
 		}
-		else if (infoCurrentSeason.value == 30){
+		else if (infoCurrentSeason.value == 3){
 			document.getElementById("info-s1").style.display = "none";
 			document.getElementById("info-s2").style.display = "none";
 			document.getElementById("info-s3").style.display = "block";
